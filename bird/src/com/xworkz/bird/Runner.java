cb4416aff62fd9db8b28e9962a90121582c92c85
xworkzodc <xworkzodc@gmail.com>
2023-07-11 14:04:52 +0530

    Added first project in eclipse

---
+++ bird/src/com/xworkz/bird/Runner.java
@@ -0,0 +1,10 @@
+package com.xworkz.bird;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("running main in Runner");
+	}
+
+}
